e5cb2203274b238dc0c20d662cf04b70515aa735
sdthomas91 <>
2023-10-10 12:58:38 +0000

    settled on a navbar design and polished it up so it is nice and responsive and displays nicely across sreens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,1 @@
-// Curved text JS for logo - found at https://circletype.labwire.ca/
-var demo4 = new CircleType(document.getElementById('logo'));
-window.addEventListener('resize', function updateRadius() {
-  demo4.radius(demo4.element.offsetWidth / 2);
-});
-updateRadius();
+console.log("I'm connected");
