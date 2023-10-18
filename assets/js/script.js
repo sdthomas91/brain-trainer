c4767c2a95336576b7d6618b9f998c0ac9abf7c2
sdthomas91 <>
2023-10-18 13:23:16 +0000

    added modals to level 1 and also implemented the first step for the JS - setting an array for the cards to be able to shuffle etc.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-// Shuffle Cards Function
-const shuffleCards = function () {
-    // On Page Load
-};
+// // Shuffle Cards Function
+// const shuffleCards = function () {
+//     // On Page Load
+// };
 
-// Start Game Function
-const startGame = function () {
-    // When first tile is clicked
-};
+// // Start Game Function
+// const startGame = function () {
+//     // When first tile is clicked
+// };
+
+//Generate card areay for use throughout 
+const cards = document.querySelectorAll('.card');
