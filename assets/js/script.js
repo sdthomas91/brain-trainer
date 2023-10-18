af3b94dc001018f2f5363d33a801c93910fa764d
sdthomas91 <>
2023-10-18 13:51:13 +0000

    fixed background issue with gradient not filling screen. Also added first function to JS - shuffle cards. Used the array that I created earlier. Makes sense for it to be the first function after game load/reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
-// // Shuffle Cards Function
-// const shuffleCards = function () {
-//     // On Page Load
-// };
 
-// // Start Game Function
-// const startGame = function () {
-//     // When first tile is clicked
-// };
+
+// Start Game Function
+const startGame = function () {
+    // When first tile is clicked
+};
 
 //Generate card areay for use throughout 
 const cards = document.querySelectorAll('.card');
+
+//Shuffle the cards using cards array
+function shuffleCards() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * cards.length);
+        card.style.order = randomPos;
+    });
+}
+
+shuffleCards(); // Shuffle cards on game load - including automstic reset after completion 
