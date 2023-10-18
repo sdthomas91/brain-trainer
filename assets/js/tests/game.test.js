116a0fc22924426e423783aae623f64697cf2f37
sdthomas91 <>
2023-10-18 13:56:01 +0000

    added basic structure for first Jest Test on Shuffle Cards

---
+++ assets/js/tests/game.test.js
@@ -0,0 +1,10 @@
+// Creating a mock object to simulate actual JS code
+const {
+    shuffleCards,
+} = require('../script');
+
+describe('Memory Game Functions', () => {
+
+    // Shuffle Cards Test
+    test('shuffleCards should shuffle the cards randomly', () => {
+    });
